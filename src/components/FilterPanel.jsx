d45a047d5597d491979d89cb95ad487116b15bcd
Andrii Ostin <ostin.andrew1@gmail.com>
2023-02-20 00:51:30 +0200

    final changes - evrth is up to reqs

--- src/components/FilterPanel.jsx
+++ src/components/FilterPanel.jsx
@@ -1,22 +1,30 @@
 import { Badge } from 'UI/Badge';
 import { Card } from 'UI/Card';
 import { Stack } from 'UI/Stack';
-
+import { useSelector, useDispatch } from 'react-redux';
+import { selectFilters } from 'store/filters/filters-selectors';
+import { removeFilter, clearFilter } from 'store/filters/filters-actions';
 
 const FilterPanel = () => {
+const currentFilters = useSelector(selectFilters);
+const dispatch = useDispatch();
+if (currentFilters.length === 0) {return null}
+
   return (
     <Card className="filter-panel">
       <div className="filter-panel-wrapper">
         <Stack>
-          <Badge variant="clearable">Frontend</Badge>
-          <Badge variant="clearable">Backend</Badge>
-          <Badge variant="clearable">React</Badge>
+          {currentFilters.map((filter) => (
+            <Badge key={filter} variant="clearable" onClick={() => {dispatch(removeFilter(filter))}}>
+              {filter}
+            </Badge>
+          ))}
         </Stack>
 
-        <button className='link'>Clear</button>
+        <button className="link" onClick={() => {dispatch(clearFilter);}}>Clear</button>
       </div>
     </Card>
-  )
+  );
 }
 
 export {FilterPanel};
--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,28 @@
 import { JobPosition } from "./JobPosition";
-import { useSelector } from "react-redux";
-import { selectAllPositions } from "store/positions/position-selectors";
+import { useSelector, useDispatch } from "react-redux";
+import { selectVisiblePositions } from "store/positions/position-selectors";
+import { addFilter } from "store/filters/filters-actions";
+import { selectFilters } from "store/filters/filters-selectors";
 
 const JobList = () => {
+  const dispatch = useDispatch();
+  const currentFilters = useSelector(selectFilters);
+  const positions = useSelector((state) =>
+    selectVisiblePositions(state, currentFilters)
+  );
 
-  const positions = useSelector(selectAllPositions);
+  const handleAddFilter =  (filter) => {
+    dispatch(addFilter(filter));
+  }
+
   return (
     <div className="job-list">
       {positions.map((item) => (
-        <JobPosition key={item.id} {...item} />
+        <JobPosition
+          key={item.id}
+          handleAddFilter={handleAddFilter}
+          {...item}
+        />
       ))}
     </div>
   );
