8da6350d82292ba2d6ddffea18125d48ada15db7
Andrii Ostin <ostin.andrew1@gmail.com>
2023-03-14 21:32:12 +0200

    persistor implemented

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
-import { store } from 'store';
+import { store, persistor } from 'store';
 import App from './App';
+import { PersistGate } from "redux-persist/integration/react";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
